b70d151847c561b8097c596caf90c0537d578b6f
Philippe Rigovanov <phil@rigovanov.ru>
2022-10-24 18:47:42 +0300

    Update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -13,7 +13,7 @@ $(function() {
 	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
+      var phone = $("input#phone").val() || "000000";
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
